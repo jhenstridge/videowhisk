d750a36f0183aff0a549efed3efed175a129a207
James Henstridge <james@jamesh.id.au>
2019-08-20 23:23:12 +0800

    server: feed data from multifdsink to HTTP connections

--- tests/server/test_avoutput.py
+++ tests/server/test_avoutput.py
@@ -4,8 +4,9 @@ import unittest
 
 import aiohttp
 import asyncio_glib
+from gi.repository import Gst
 
-from videowhisk.server import avoutput
+from videowhisk.server import avoutput, messagebus
 
 
 class AVOutputTests(unittest.TestCase):
@@ -13,19 +14,62 @@ class AVOutputTests(unittest.TestCase):
     def setUp(self):
         self.loop = asyncio_glib.GLibEventLoop()
         self.loop.add_signal_handler(signal.SIGINT, self.loop.stop)
-        self.server = avoutput.AVOutputServer(("127.0.0.1", 0), self.loop)
+        self.bus = messagebus.MessageBus(self.loop)
+        self.server = avoutput.AVOutputServer(
+            self.bus, ("127.0.0.1", 0), self.loop)
 
     def tearDown(self):
         self.loop.run_until_complete(self.server.close())
+        self.loop.run_until_complete(self.bus.close())
 
-    def test_server(self):
+    def make_audio_source(self):
+        pipeline = Gst.parse_launch("""
+            audiotestsrc freq=440 !
+            audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=48000 !
+            interaudiosink channel=source.audio
+        """)
+        pipeline.set_state(Gst.State.PLAYING)
+        self.addCleanup(pipeline.set_state, Gst.State.NULL)
+        self.loop.create_task(self.bus.post(messagebus.AudioSourceAdded("source.audio", "127.0.0.1")))
+
+    def make_video_source(self):
+        pipeline = Gst.parse_launch("""
+            videotestsrc !
+            video/x-raw,format=I420,width=1920,height=1080,framerate=25/1,pixel-aspect-ratio=1/1,interlace-mode=progressive !
+            intervideosink channel=source.video
+        """)
+        pipeline.set_state(Gst.State.PLAYING)
+        self.addCleanup(pipeline.set_state, Gst.State.NULL)
+        self.loop.create_task(self.bus.post(messagebus.VideoSourceAdded("source.video", "127.0.0.1")))
+
+    def test_audio(self):
+        self.make_audio_source()
+        headers = None
         body = None
         async def make_request():
-            nonlocal body
+            nonlocal headers, body
             async with aiohttp.ClientSession() as session:
-                url = "http://127.0.0.1:{}/foo".format(
+                url = "http://127.0.0.1:{}/source.audio".format(
                     self.server.local_addr()[1])
                 async with session.get(url) as response:
-                    body = await response.text()
+                    headers = response.headers
+                    body = await response.content.read(100)
         self.loop.run_until_complete(make_request())
-        self.assertEqual(body, "Good\n")
+        self.assertEqual(headers["Content-Type"], "audio/x-matroska")
+        self.assertEqual(body[:4], b"\x1A\x45\xDF\xA3")
+
+    def test_video(self):
+        self.make_video_source()
+        headers = None
+        body = None
+        async def make_request():
+            nonlocal headers, body
+            async with aiohttp.ClientSession() as session:
+                url = "http://127.0.0.1:{}/source.video".format(
+                    self.server.local_addr()[1])
+                async with session.get(url) as response:
+                    headers = response.headers
+                    body = await response.content.read(100)
+        self.loop.run_until_complete(make_request())
+        self.assertEqual(headers["Content-Type"], "video/x-matroska")
+        self.assertEqual(body[:4], b"\x1A\x45\xDF\xA3")
--- videowhisk/server/avoutput.py
+++ videowhisk/server/avoutput.py
@@ -1,18 +1,22 @@
 import asyncio
 import socket
 
-#from gi.repository import Gst
+from gi.repository import Gst
 try:
     from http_parser.parser import HttpParser
 except ImportError:
     from http_parser.pyparser import HttpParser
 
+from . import messagebus
+
 class AVOutputServer:
 
-    def __init__(self, address, loop):
+    def __init__(self, bus, address, loop):
         self._loop = loop
         self._closed = False
+        bus.add_consumer(messagebus.SourceMessage, self.handle_message)
         self._connections = {}
+        self._monitors = {}
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.setblocking(False)
         self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
@@ -30,6 +34,8 @@ class AVOutputServer:
 
         for conn in list(self._connections.values()):
             await conn.close()
+        for monitor in list(self._monitors.values()):
+            await monitor.close()
 
     def local_addr(self):
         return self._sock.getsockname()
@@ -40,12 +46,107 @@ class AVOutputServer:
             conn = AVOutputConnection(sock, self)
             self._connections[sock.fileno()] = conn
 
-    def _connection_ready(self, conn, path):
-        print("Ready to serve", path)
+    async def handle_message(self, queue):
+        while True:
+            message = await queue.get()
+            if isinstance(message, messagebus.AudioSourceAdded):
+                print("Adding audio monitor", message.channel)
+                monitor = AVMonitor(message.channel, False, self)
+                self._monitors[message.channel] = monitor
+                monitor.start()
+            elif isinstance(message, messagebus.VideoSourceAdded):
+                print("Adding video monitor", message.channel)
+                monitor = AVMonitor(message.channel, True, self)
+                self._monitors[message.channel] = monitor
+                monitor.start()
+            elif isinstance(message, (messagebus.AudioSourceRemoved,
+                                      messagebus.VideoSourceRemoved)):
+                print("Removing monitor", message.channel)
+                monitor = self._monitors.pop(message.channel, None)
+                if monitor is not None:
+                    await monitor.close()
+            queue.task_done()
+
+    def get_monitor(self, channel):
+        return self._monitors.get(channel)
 
     def _connection_closed(self, conn):
         self._connections.pop(conn.fileno())
 
+    async def _monitor_remove_fd(self, fileno):
+        conn = self._connections.get(fileno)
+        if conn is not None:
+            await conn.close()
+
+
+class AVMonitor:
+    def __init__(self, channel, is_video, server):
+        self._closed = False
+        self._channel = channel
+        self.is_video = is_video
+        self._server = server
+        self._loop = server._loop
+        self._filenos = set()
+        self.make_pipeline()
+
+    async def close(self):
+        if self._closed:
+            return
+        self._closed = True
+        self.destroy_pipeline()
+        for fileno in self._filenos:
+            await self._server._monitor_remove_fd(fileno)
+
+    def make_pipeline(self):
+        self._pipeline = Gst.Pipeline("monitor.{}".format(self._channel))
+        self._pipeline.use_clock(Gst.SystemClock.obtain())
+
+        if self.is_video:
+            src = Gst.ElementFactory.make("intervideosrc")
+            caps = Gst.Caps.from_string("video/x-raw,format=I420,width=1920,height=1080,framerate=25/1,pixel-aspect-ratio=1/1,interlace-mode=progressive")
+        else:
+            src = Gst.ElementFactory.make("interaudiosrc")
+            caps = Gst.Caps.from_string("audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=48000")
+
+        src.props.channel = self._channel
+        queue = Gst.ElementFactory.make("queue", "srcqueue")
+        mux = Gst.ElementFactory.make("matroskamux")
+        mux.props.streamable = True
+        mux.props.writing_app = "videowhisk"
+        self._sink = Gst.ElementFactory.make("multifdsink")
+        self._sink.props.blocksize = 1048576
+        self._sink.props.buffers_max = 500
+        self._sink.props.sync_method = 1 # "next-keyframe"
+
+        self._pipeline.add(src, queue, mux, self._sink)
+        src.link_filtered(queue, caps)
+        queue.link(mux)
+        mux.link(self._sink)
+
+        self._sink.connect("client-removed", self.on_client_removed)
+        self._sink_signal_id = self._sink.connect(
+            "client-fd-removed", self.on_client_fd_removed)
+
+    def destroy_pipeline(self):
+        self._pipeline.set_state(Gst.State.NULL)
+        self._sink.disconnect(self._sink_signal_id)
+        self._sink = None
+        self._pipeline = None
+
+    def start(self):
+        self._pipeline.set_state(Gst.State.PLAYING)
+
+    def add_fd(self, fileno):
+        self._filenos.add(fileno)
+        self._sink.emit("add", fileno)
+
+    def on_client_removed(self, sink, fileno, status):
+        print("client-removed", fileno, status.value_nick)
+
+    def on_client_fd_removed(self, sink, fileno):
+        self._filenos.remove(fileno)
+        self._loop.call_soon_threadsafe(
+            self._loop.create_task, self._server._monitor_remove_fd(fileno))
 
 
 class AVOutputConnection:
@@ -76,18 +177,32 @@ class AVOutputConnection:
             if nparsed != len(data):
                 break
 
-        self._sock.shutdown(socket.SHUT_RD)
+        if not (p.is_message_complete() and p.get_method() in ("GET", "HEAD")):
+            response = (b"HTTP/1.1 400 Bad Request\r\n"
+                        b"Content-Type: text/plain\r\n"
+                        b"\r\n"
+                        b"Bad Request\n")
+            await self._loop.sock_sendall(self._sock, response)
+            await self.close()
+            return
 
-        if p.is_message_complete() and p.get_method() == "GET":
-            self._server._connection_ready(self, p.get_path())
-            response = (b"HTTP/1.0 200 OK\r\n"
+        channel = p.get_path().strip("/")
+        monitor = self._server.get_monitor(channel)
+        if monitor is None:
+            response = (b"HTTP/1.1 404 Not Found\r\n"
                         b"Content-Type: text/plain\r\n"
-                        b"\r\n"
-                        b"Good\n")
+                        b"\r\n")
+            await self._loop.sock_sendall(self._sock, response)
+            await self.close()
+            return
+
+        response = b"HTTP/1.1 200 OK\r\n"
+        if monitor.is_video:
+            response += b"Content-Type: video/x-matroska\r\n\r\n"
         else:
-            response = (b"HTTP/1.0 400 Bad Request\r\n"
-                        b"Content-Type: text/plain\r\n"
-                        b"\r\n"
-                        b"Bad\n")
+            response += b"Content-Type: audio/x-matroska\r\n\r\n"
         await self._loop.sock_sendall(self._sock, response)
-        await self.close()
+        if p.get_method() == "HEAD":
+            await self.close()
+            return
+        monitor.add_fd(self._sock.fileno())
