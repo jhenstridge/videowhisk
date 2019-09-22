2b3ae36df91d16f33aef4f1517914042b6b291ae
James Henstridge <james@jamesh.id.au>
2019-09-22 10:06:56 +0200

    server: set version requirement for GstNet

--- videowhisk/server/__main__.py
+++ videowhisk/server/__main__.py
@@ -7,6 +7,7 @@ import asyncio_glib
 # We need to initialise gst-python before importing our own code
 import gi
 gi.require_version('Gst', '1.0')
+gi.require_version('GstNet', '1.0')
 from gi.repository import Gst
 Gst.init(None)
 
